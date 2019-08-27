b7e4df081f983c15452f83c0a7aa4892701ebfa9
PajarKharisma <pajarkharisma@hotmail.com>
2019-08-27 11:16:03 +0700

    add remove link

--- src/modules/PreProcess.py
+++ src/modules/PreProcess.py
@@ -4,18 +4,21 @@ import re
 
 REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)|(\@)|(\\\)|(\.)")
 REPLACE_NO_SPACE = re.compile("(\#)|(\.)|(\;)|(\:)|(\!)|(\')|(\?)|(\,)|(\")|(\()|(\))|(\[)|(\])|(\@)|(\/)|(\\\)")
+LINK = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
 
 def getResult(reviews):
+    reviews = [removeLink(line) for line in reviews]
+
     default_stop_words = nltk.corpus.stopwords.words('indonesian-sentimen')
     stopwords = set(default_stop_words)
 
     reviews = [REPLACE_WITH_SPACE.sub(" ", line) for line in reviews]
     reviews = [REPLACE_NO_SPACE.sub("", line.lower()) for line in reviews]
-    reviews = [RemoveStopWords(line,stopwords) for line in reviews]
+    reviews = [removeStopWords(line,stopwords) for line in reviews]
     
     return reviews
 
-def RemoveStopWords(line, stopwords):
+def removeStopWords(line, stopwords):
     words = []
     for word in line.split(" "):
         word = word.strip()
@@ -23,3 +26,15 @@ def RemoveStopWords(line, stopwords):
             words.append(word)
 
     return " ".join(words)
+
+def removeLink(s):
+    list = s.split()
+    i = 0
+    while i < len(list):
+        if list[i].startswith('https:') or list[i].startswith('@') or list[i].startswith('http:'):
+            del list[i]
+        else:
+            i+=1
+
+    s = ' '.join(list)
+    return s
