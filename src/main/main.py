7a446ccbbc7cad3240ea8796bf5d743b969a81f4
PajarKharisma <pajarkharisma@hotmail.com>
2019-11-13 22:02:55 +0700

    update

--- src/main/main.py
+++ src/main/main.py
@@ -77,7 +77,6 @@ def crossValidation1(dataInput, embeddingMatrix, maxDataLenght):
         embeddingVectorLength = 200
         model = Sequential()
         model.add(Embedding(input_dim=NUM_OF_ATTRIBUTES, output_dim=embeddingVectorLength, weights=[embeddingMatrix], input_length=maxDataLenght, trainable=False))
-        # model.add(Flatten())
         model.add(Bidirectional(LSTM(NUM_OF_NEURONS, return_sequences=False), merge_mode="sum"))
         model.add(Dropout(0.2))
         model.add(Dense(1, activation='sigmoid', kernel_regularizer=regularizers.l2(0.001)))
@@ -142,7 +141,10 @@ def crossValidation1(dataInput, embeddingMatrix, maxDataLenght):
     plt.show()
 
 # Bidirectional LSTM 3 layer
-def crossValidation2(NUM_OF_ATTRIBUTES, dataInput, embeddingMatrix):
+def crossValidation2(dataInput, embeddingMatrix, maxDataLenght):
+    global logFile
+    logFile += log.summaryLog(method='Bi-LSTM 3 Layer', numEpochs=NUM_OF_EPOCHS, numAttributes=NUM_OF_ATTRIBUTES, numNeurons=NUM_OF_NEURONS, sg=1) + '\n'
+    logFile += '=' * 30 + '\n\n'
     seed = 7
     np.random.seed(seed)
 
@@ -151,7 +153,6 @@ def crossValidation2(NUM_OF_ATTRIBUTES, dataInput, embeddingMatrix):
     Y = np.array(Y)
 
     # pad Sentences with Keras
-    maxDataLenght = 30
     X = sequence.pad_sequences(X, maxlen=maxDataLenght)
 
     # set k value for cross validation
@@ -163,24 +164,23 @@ def crossValidation2(NUM_OF_ATTRIBUTES, dataInput, embeddingMatrix):
 
     # cross validation process
     for train, test in kfold.split(X, Y):
-    #     # build model
+       # build model
         embeddingVectorLength = 200
         model = Sequential()
         model.add(Embedding(input_dim=NUM_OF_ATTRIBUTES, output_dim=embeddingVectorLength, weights=[embeddingMatrix], input_length=maxDataLenght, trainable=False))
-        # model.add(Flatten())
-        model.add(Bidirectional(LSTM(100, return_sequences=True)))
-        model.add(Bidirectional(LSTM(100, return_sequences=True)))
-        model.add(Bidirectional(LSTM(100, return_sequences=False)))
+        model.add(Bidirectional(LSTM(NUM_OF_NEURONS, return_sequences=True), merge_mode="sum"))
+        model.add(Bidirectional(LSTM(NUM_OF_NEURONS, return_sequences=True), merge_mode="sum"))
+        model.add(Bidirectional(LSTM(NUM_OF_NEURONS, return_sequences=False), merge_mode="sum"))
         model.add(Dropout(0.2))
         model.add(Dense(1, activation='sigmoid', kernel_regularizer=regularizers.l2(0.001)))
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=[
             'accuracy',
-            cm.f1_m,
-            cm.precision_m,
-            cm.recall_m
+            km.binary_f1_score(),
+            km.binary_precision(),
+            km.binary_recall()
         ])
         # print(model.summary())
-        model.fit(X[train], Y[train], validation_data=(X[test], Y[test]), epochs=20, batch_size=256, verbose=0)
+        model.fit(X[train], Y[train], validation_data=(X[test], Y[test]), epochs=NUM_OF_EPOCHS, batch_size=256, verbose=0)
         
         # evaluate the model
         # 1 = accuracy
@@ -188,23 +188,27 @@ def crossValidation2(NUM_OF_ATTRIBUTES, dataInput, embeddingMatrix):
         # 3 = precission
         # 4 = recall
         # model = load_model('model.weights.best.hdf5', custom_objects={'f1_m':cm.f1_m, 'precision_m':cm.precision_m, 'recall_m':cm.recall_m})
-        scores = model.evaluate(X[test], Y[test], verbose=0)
+        scores = model.evaluate(X[train], Y[train], verbose=0)
         for i in range(1,5):
+            logFile += "%s : %.2f%%\n" % (model.metrics_names[i], scores[i]*100)
             print("%s : %.2f%%" % (model.metrics_names[i], scores[i]*100))
             cvscores[i].append(scores[i]*100)
+        logFile += '=' * 30 + '\n'
         print("========================================================")
 
+    logFile += 'Mean : \n'
     print("Mean : ")
     for i in range(1,5):
         metricName = ''
         if i == 1:
             metricName = 'acc'
         elif i == 2:
-            metricName = 'f1_m'
+            metricName = 'f1_score'
         elif i == 3:
-            metricName = 'precision_m'
+            metricName = 'precision'
         elif i == 4:
-            metricName = 'recall_m'
+            metricName = 'recall'
+        logFile += "%s : %.2f%% (+/- %.2f%%)\n" % (metricName, np.mean(cvscores[i]), np.std(cvscores[i]))
         print("%s : %.2f%% (+/- %.2f%%)" % (metricName, np.mean(cvscores[i]), np.std(cvscores[i])))
 
     # show result to matplotlib
@@ -230,6 +234,96 @@ def crossValidation2(NUM_OF_ATTRIBUTES, dataInput, embeddingMatrix):
     plt.show()
 
 # LSTM 1 layer
+def crossValidation3(dataInput, embeddingMatrix, maxDataLenght):
+    global logFile
+    logFile += log.summaryLog(method='LSTM 1 Layer', numEpochs=NUM_OF_EPOCHS, numAttributes=NUM_OF_ATTRIBUTES, numNeurons=NUM_OF_NEURONS, sg=1) + '\n'
+    logFile += '=' * 30 + '\n\n'
+    seed = 7
+    np.random.seed(seed)
+
+    # split data and label
+    X, Y = list(zip(*dataInput))
+    Y = np.array(Y)
+
+    # pad Sentences with Keras
+    X = sequence.pad_sequences(X, maxlen=maxDataLenght)
+
+    # set k value for cross validation
+    split = 10
+    kfold = StratifiedKFold(n_splits=split, shuffle=True, random_state=seed)
+    cvscores = []
+    for i in range(0,5):
+        cvscores.append([])
+
+    # cross validation process
+    for train, test in kfold.split(X, Y):
+       # build model
+        embeddingVectorLength = 200
+        model = Sequential()
+        model.add(Embedding(input_dim=NUM_OF_ATTRIBUTES, output_dim=embeddingVectorLength, weights=[embeddingMatrix], input_length=maxDataLenght, trainable=False))
+        model.add(LSTM(NUM_OF_NEURONS, return_sequences=False))
+        model.add(Dropout(0.2))
+        model.add(Dense(1, activation='sigmoid', kernel_regularizer=regularizers.l2(0.001)))
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=[
+            'accuracy',
+            km.binary_f1_score(),
+            km.binary_precision(),
+            km.binary_recall()
+        ])
+        # print(model.summary())
+        model.fit(X[train], Y[train], validation_data=(X[test], Y[test]), epochs=NUM_OF_EPOCHS, batch_size=256, verbose=0)
+        
+        # evaluate the model
+        # 1 = accuracy
+        # 2 = f1_score
+        # 3 = precission
+        # 4 = recall
+        # model = load_model('model.weights.best.hdf5', custom_objects={'f1_m':cm.f1_m, 'precision_m':cm.precision_m, 'recall_m':cm.recall_m})
+        scores = model.evaluate(X[train], Y[train], verbose=0)
+        for i in range(1,5):
+            logFile += "%s : %.2f%%\n" % (model.metrics_names[i], scores[i]*100)
+            print("%s : %.2f%%" % (model.metrics_names[i], scores[i]*100))
+            cvscores[i].append(scores[i]*100)
+        logFile += '=' * 30 + '\n'
+        print("========================================================")
+
+    logFile += 'Mean : \n'
+    print("Mean : ")
+    for i in range(1,5):
+        metricName = ''
+        if i == 1:
+            metricName = 'acc'
+        elif i == 2:
+            metricName = 'f1_score'
+        elif i == 3:
+            metricName = 'precision'
+        elif i == 4:
+            metricName = 'recall'
+        logFile += "%s : %.2f%% (+/- %.2f%%)\n" % (metricName, np.mean(cvscores[i]), np.std(cvscores[i]))
+        print("%s : %.2f%% (+/- %.2f%%)" % (metricName, np.mean(cvscores[i]), np.std(cvscores[i])))
+
+    # show result to matplotlib
+    plt.style.use('classic')
+    fig, axs = plt.subplots(2, 2, gridspec_kw={'hspace': 0.5, 'wspace': 0.5})
+
+    iteration = [*range(1,11)]
+    fig.suptitle('Result Data')
+    (ax1, ax2), (ax3, ax4) = axs
+
+    ax1.plot(iteration, cvscores[1])
+    ax1.set_ylabel('Accuracy (%)')
+
+    ax2.plot(iteration, cvscores[2], 'tab:green')
+    ax2.set_ylabel('F1 Measurement (%)')
+
+    ax3.plot(iteration, cvscores[3], 'tab:orange')
+    ax3.set_ylabel('Precission (%)')
+
+    ax4.plot(iteration, cvscores[4], 'tab:red')
+    ax4.set_ylabel('Recall (%)')
+
+    plt.show()
+
 def crossValidation3(NUM_OF_ATTRIBUTES, dataInput, embeddingMatrix):
     seed = 7
     np.random.seed(seed)
@@ -351,30 +445,6 @@ def splitValidation(NUM_OF_ATTRIBUTES, dataInput):
     hst = model.fit(xTrain, yTrain, validation_data=(xTest, yTest), epochs=10, batch_size=64)
 
     plt.style.use('classic')
-    # fig, axs = plt.subplots(2, 2, gridspec_kw={'hspace': 0.5, 'wspace': 0.5})
-
-    # fig.suptitle('Result Data')
-    # (ax1, ax2), (ax3, ax4) = axs
-
-    # ax1.plot(hst.history['acc'], label='train')
-    # ax1.plot(hst.history['val_acc'], label='validation')
-    # ax1.set_xlabel('Accuracy')
-    # ax1.legend()
-
-    # ax2.plot(hst.history['f1_m'], label='train')
-    # ax2.plot(hst.history['val_f1_m'], label='validation')
-    # ax2.set_xlabel('F Measurement')
-    # ax2.legend()
-
-    # ax3.plot(hst.history['precision_m'], label='train')
-    # ax3.plot(hst.history['val_precision_m'], label='validation')
-    # ax3.set_xlabel('Precision')
-    # ax3.legend()
-
-    # ax4.plot(hst.history['recall_m'], label='train')
-    # ax4.plot(hst.history['val_recall_m'], label='validation')
-    # ax4.set_xlabel('Recall')
-    # ax4.legend()
 
     print(np.mean(hst.history['acc']))
     plt.title('Accuracy')
@@ -384,7 +454,7 @@ def splitValidation(NUM_OF_ATTRIBUTES, dataInput):
     plt.show()
     plt.show()
 
-def nbMethod(NUM_OF_ATTRIBUTES, dataInput):
+def nbMethod(NUM_OF_ATTRIBUTES, dataInput, maxDataLenght):
     # split dataset for train and test
     train, test = train_test_split(dataInput, test_size=0.2)
     xTrain, yTrain = list(zip(*train))
@@ -394,7 +464,6 @@ def nbMethod(NUM_OF_ATTRIBUTES, dataInput):
     yTrain = np.array(yTrain)
     yTest = np.array(yTest)
 
-    maxDataLenght = 30
     xTrain = sequence.pad_sequences(xTrain, maxlen=maxDataLenght) 
     xTest = sequence.pad_sequences(xTest, maxlen=maxDataLenght)
 
@@ -501,7 +570,6 @@ def main():
     # 1 - Bidirectional LSTM 1 Layer
     # 2 - Bidirectional LSTM 3 Layer
     # 3 - LSTM 1 Layer
-    # check(NUM_OF_ATTRIBUTES, dataLabeledInt, embeddingMatrix)
     crossValidation1(dataLabeledInt, embeddingMatrix, maxDataLenght)
     # splitValidation(NUM_OF_ATTRIBUTES, dataLabeledInt)
     # createModel(NUM_OF_ATTRIBUTES, dataLabeledInt)
